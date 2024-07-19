5b07e8e05164a360b4acad6cf86a7fa61d2c6a92
MathotaGamma <159739543+MathotaGamma@users.noreply.github.com>
2024-07-19 23:28:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def handle_all_error(e):
 def Notify_pushcode_sw():
   file_k = ""
   with open('static/Notify/pushcode_sw.js',mode='r') as f:
-    file_k = f.readlines()
+    file_k = f.read()
   return file_k
 
 @app.route('/')
