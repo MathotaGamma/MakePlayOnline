690847f0c08bdd4a92a198a601a52db392fd7eda
MathotaGamma <159739543+MathotaGamma@users.noreply.github.com>
2024-08-10 08:17:50 +0900

    Update play_script.js

--- static/LIFESHAVE/play_script.js
+++ static/LIFESHAVE/play_script.js
@@ -227,7 +227,7 @@ function Main(){
 
   alert('1');
   
-  loader.load("../../../Staatic/LIFESHAVE/humancubist.gltf", function (human) {
+  loader.load("{{url_for('static',filename='LIFESHAVE/humancubist.gltf') | tojson}}", function (human) {
     alert(human);
     loader.load("../../../Static/LIFESHAVE/lifeshave_floor.gltf", function (floor) {
       alert(floor);
