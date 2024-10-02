36e11a4da31d1dbd446b0ca73558c1b1100a5c98
MathotaGamma <159739543+MathotaGamma@users.noreply.github.com>
2024-10-02 20:03:28 +0900

    Create id.js

---
+++ static/id.js
@@ -0,0 +1,11 @@
+let date = localStorage.getItem('created_date');
+
+let main_date = {created_date | tojson};
+alert(main_date);
+
+function id_post(){
+  alert('null');
+}
+if(date == null){
+  id_post();
+}
