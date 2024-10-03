9f81a94bd18d81dc4d9946da7e758982ecdf2dc2
MathotaGamma <159739543+MathotaGamma@users.noreply.github.com>
2024-10-03 10:05:50 +0900

    Update id.js

--- static/id.js
+++ static/id.js
@@ -4,7 +4,24 @@ let main_date = {{created_date | tojson}};
 alert(main_date);
 
 function id_post(){
-  alert('null');
+  var form = document.createElement('form');
+  form.action = '/sign';
+  form.method = 'POST';
+    
+  // body に追加
+  document.body.append(form);
+    
+  // formdta イベントに関数を登録(submit する直前に発火)
+  form.addEventListener('formdata', (e) => {
+    var fd = e.formData;
+      
+    // データをセット
+    fd.set('email', 'dummy@gmail.com');
+    fd.set('password', '1234abcd');
+  });
+
+  // submit
+  form.submit();
 }
 if(date == null){
   id_post();
